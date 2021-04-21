354fd1687ed72867b0552df8b33b48fe36b3ac29
philling83 <philipling83@gmail.com>
2021-04-21 14:35:27 -0400

    Refine Track query

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,5 +1,22 @@
 import { gql, useQuery } from '@apollo/client';
 
+// const getToken = async () => {
+  
+//   const result= await fetch('http://accounts.spotify.com/api/token', {
+//     method: "POST",
+//     headers: {
+//       'Content-Type' : 'application/x-www-form-urlencoded',
+//       'Authorization' : 'Basic ' + btoa(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET)
+//     },
+//     body: 'grant_type=client_credentials'
+//   });
+
+//   const data = await result.json();
+//   return data.access_token;
+// }
+
+// console.log(getToken())
+
 const GET_STEPZEN = gql`
   query MyHelloWorldQuery {
     helloWorld {
