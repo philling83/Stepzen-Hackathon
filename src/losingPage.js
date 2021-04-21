8624dca75c9caefc7fe8065c83fc1f51262fb06f
IvanGent <gentilleivan@gmail.com>
2021-04-21 09:03:20 -0400

    styling the github image

--- src/losingPage.js
+++ src/losingPage.js
@@ -2,20 +2,30 @@ import React from 'react';
 
 const LosingPage = () => {
 
+    const imageStyle = {
+        width: '50px',
+    }
+
     return (
         <section className='losingPage'>
-            <a href="https://github.com/philling83">
-                Philip's Github
-                <img src='./images/Github.png' alt='github' />
-            </a>
-            <a href="https://github.com/sal-wav">
-                Salina's Github
-                <img src='./images/Github.png' alt='github' />
-            </a>
-            <a href="https://github.com/IvanGent">
-                Ivan's Github
-                <img src='./images/Github.png' alt='github' />
-            </a>
+            <div>
+                Game Over!
+            </div>
+            <button>Play Again</button>
+            <div className='links'>
+                <a href="https://github.com/philling83">
+                    Philip's Github
+                    <img style={imageStyle} src='./images/Github.png' alt='github' />
+                </a>
+                <a href="https://github.com/sal-wav">
+                    Salina's Github
+                    <img style={imageStyle} src='./images/Github.png' alt='github' />
+                </a>
+                <a href="https://github.com/IvanGent">
+                    Ivan's Github
+                    <img style={imageStyle} src='./images/Github.png' alt='github' />
+                </a>
+            </div>
         </section>
     )
 };
