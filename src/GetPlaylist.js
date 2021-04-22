821f88e86f87c03f03b1a678bf6495be79f5b72d
philling83 <philipling83@gmail.com>
2021-04-22 09:04:17 -0400

    Add high score to localStorage, update styling

--- src/GetPlaylist.js
+++ src/GetPlaylist.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 
@@ -16,7 +16,11 @@ function GetPlaylist({ playlist, tracks }) {
 
   console.log(leftSongIndex)
   console.log(gameFeed)
-  
+
+  useEffect(() => {
+    setHighScore(highScore => highScore = Number(localStorage.getItem("highscore")));
+  }, []);
+
   const checkHigher = () => {
     if (gameFeed.song2.track.popularity > gameFeed.song1.track.popularity) {
       setLeftSongIndex(leftSongIndex => leftSongIndex + 1);
@@ -25,9 +29,11 @@ function GetPlaylist({ playlist, tracks }) {
         song1: { artist: tracks[leftSongIndex].artist, image: playlist.images[leftSongIndex], track: tracks[leftSongIndex] },
         song2: { artist: tracks[rightSongIndex].artist, image: playlist.images[rightSongIndex], track: tracks[rightSongIndex] }
       });
-      setHighScore(highScore => highScore + 1);
       setCurrentHighScore(currentHighScore => currentHighScore + 1);
     } else {
+      if (currentHighScore > localStorage.getItem('highscore')) {
+        localStorage.setItem('highscore', currentHighScore);
+      }
       history.push('/lost');
     };
   };
@@ -40,9 +46,11 @@ function GetPlaylist({ playlist, tracks }) {
         song1: { artist: tracks[leftSongIndex].artist, image: playlist.images[leftSongIndex], track: tracks[leftSongIndex] },
         song2: { artist: tracks[rightSongIndex].artist, image: playlist.images[rightSongIndex], track: tracks[rightSongIndex] }
       });
-      setHighScore(highScore => highScore + 1);
       setCurrentHighScore(currentHighScore => currentHighScore + 1);
     } else {
+      if (currentHighScore > localStorage.getItem('highscore')) {
+        localStorage.setItem('highscore', currentHighScore);
+      }
       history.push('/lost');
     };
   };
@@ -55,9 +63,11 @@ function GetPlaylist({ playlist, tracks }) {
         song1: { artist: tracks[leftSongIndex].artist, image: playlist.images[leftSongIndex], track: tracks[leftSongIndex] },
         song2: { artist: tracks[rightSongIndex].artist, image: playlist.images[rightSongIndex], track: tracks[rightSongIndex] }
       });
-      setHighScore(highScore => highScore + 1);
       setCurrentHighScore(currentHighScore => currentHighScore + 1);
     } else {
+      if (currentHighScore > localStorage.getItem('highscore')) {
+        localStorage.setItem('highscore', currentHighScore);
+      }
       history.push('/lost');
     };
   };
@@ -67,15 +77,15 @@ function GetPlaylist({ playlist, tracks }) {
       <div className="game_grid">
         <div className="song_tile">
           <div className="left_song">
-            <img src={gameFeed.song1.image.url}></img>
+            <img className="game_image" src={gameFeed.song1.image.url}></img>
             <div className="text">{gameFeed.song1.track.name} by {gameFeed.song1.artist}</div>
             <div className="text">Has a Popularity Score of: </div>
-            <div className="text">{gameFeed.song1.track.popularity}</div>
+            <div className="text popularity">{gameFeed.song1.track.popularity}</div>
           </div>
         </div>
         <div className="circle">VS</div>
         <div className="right_song">
-          <img src={gameFeed.song2.image.url}></img>
+          <img className="game_image" src={gameFeed.song2.image.url}></img>
           <div className="text">{gameFeed.song2.track.name} by {gameFeed.song2.artist}</div>
           <div>
             <button className="game_button" onClick={checkHigher}>Higher</button>
@@ -89,8 +99,8 @@ function GetPlaylist({ playlist, tracks }) {
           <div className="text">Popularity Score than {gameFeed.song1.track.name}</div>
         </div>
         <div className="high_score_container">
-          <div className="high_score">High Score: {highScore}</div>
-          <div className="current_high_score">Current High Score: {currentHighScore}</div>
+          <div className="score high_score">High Score: {highScore}</div>
+          <div className="score current_high_score">Current High Score: {currentHighScore}</div>
         </div>
       </div>
     </>
