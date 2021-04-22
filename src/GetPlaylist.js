c5ed02483967ad56c98898fe30badc309ec94f9c
philling83 <philipling83@gmail.com>
2021-04-22 00:13:26 -0400

    Work on game state change

--- src/GetPlaylist.js
+++ src/GetPlaylist.js
@@ -3,14 +3,25 @@ import React, { useState } from 'react';
 
 function GetPlaylist({ playlist, tracks }) {
 
-  let i = 0;
-  let j = 1;
+  // let i = 0;
+  // let j = 1;
 
+  const [leftSongIndex, setLeftSongIndex] = useState(0);
+  const [rightSongIndex, setRightSongIndex] = useState(1);
   const [trackFeed, setTrackFeed] = useState({artists: playlist.artists, images: playlist.images, tracks});
-  const [gameFeed, setGameFeed] = useState({ song1: {artist: playlist.artists[i], image: playlist.images[i], track: tracks[i]},
-                                             song2: {artist: playlist.artists[j], image: playlist.images[j], track: tracks[j]}});
+  const [gameFeed, setGameFeed] = useState({ song1: {artist: playlist.artists[leftSongIndex], image: playlist.images[leftSongIndex], track: tracks[leftSongIndex]},
+                                             song2: {artist: playlist.artists[rightSongIndex], image: playlist.images[rightSongIndex], track: tracks[rightSongIndex]}});
   const [highScore, setHighScore] = useState(0);
   const [currentHighScore, setCurrentHighScore] = useState(0);
+
+  const checkHigher = () => {
+    setLeftSongIndex(leftSongIndex + 1);
+    setGameFeed({
+      song1: { artist: playlist.artists[leftSongIndex], image: playlist.images[leftSongIndex], track: tracks[leftSongIndex] },
+      song2: { artist: playlist.artists[rightSongIndex], image: playlist.images[rightSongIndex], track: tracks[rightSongIndex] }
+    })
+    console.log(leftSongIndex)
+  }
 
   return (
     <>
@@ -19,18 +30,16 @@ function GetPlaylist({ playlist, tracks }) {
           <div className="left_song">
             <img src={gameFeed.song1.image.url}></img>
             <div className="text">{gameFeed.song1.track.name} by {gameFeed.song1.artist.name}</div>
-            {/* <div className="text">{gameFeed.song1.artist.name}</div> */}
             <div className="text">Has a Popularity Score of: </div>
             <div className="text">{gameFeed.song1.track.popularity}</div>
           </div>
         </div>
         <div className="circle">VS</div>
-        {/* <div className="song_tile"> */}
         <div className="right_song">
           <img src={gameFeed.song2.image.url}></img>
           <div className="text">{gameFeed.song2.track.name} by {gameFeed.song2.artist.name}</div>
           <div>
-            <button className="game_button">Higher</button>
+            <button className="game_button" onClick={checkHigher}>Higher</button>
           </div>
           <div>
             <button className="game_button">Lower</button>
