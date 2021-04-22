2b722dfdce3283226af201804ea740ad17c9600e
philling83 <philipling83@gmail.com>
2021-04-22 03:07:21 -0400

    Edit query and fix routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import {useState} from 'react';
-// import {Route} from 'react-router-dom';
-// import HelloWorld from './HelloWorld';
 import LosingPage from './losingPage';
-// import GetTrack from './GetTrack';
 import React, { NavLink } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { gql, useQuery } from '@apollo/client';
@@ -16,9 +13,6 @@ const GET_PLAYLIST = gql`
   query PlaylistQuery {
     playlistById {
       id
-      artists {
-        name
-      }
       images {
         url
       }
@@ -26,6 +20,7 @@ const GET_PLAYLIST = gql`
     tracks {
       name
       popularity
+      artist
     }
   }
 `;
@@ -33,33 +28,27 @@ const GET_PLAYLIST = gql`
 function App() {
   const [track1, setTrack1] = useState();
   const [track2, setTrack2] = useState();
-  // const { loading, error, data } = useQuery(GET_TRACK);
 
   const { loading, error, data } = useQuery(GET_PLAYLIST);
 
-  // if (error) return <p>{JSON.stringify(error)}</p>;
-  // if (loading) return <p>Loading ...</p>;
+  if (error) return <p>{JSON.stringify(error)}</p>;
+  if (loading) return <p>Loading ...</p>;
 
-  // const playlist = data.playlistById
   const playlist = data.playlistById
   const tracks = data.tracks
 
   return (
-    <div className="App">
-      <Route path='/' exact={true}>
-        <BrowserRouter>
-          <Route path="/" exact={true}>
-            <Splash />
-          </Route>
-        <Route path="/game" exact={true}>
-          <GetPlaylist playlist={playlist} tracks={tracks} />
-        </Route>
-        </BrowserRouter>
+    <BrowserRouter>
+      <Route path="/" exact={true}>
+        <Splash />
       </Route>
-        <Route path='/lost'>
-          <LosingPage track2={track2} />
-        </Route>
-    </div>
+      <Route path="/game" exact={true}>
+        <GetPlaylist playlist={playlist} tracks={tracks} />
+      </Route>
+      <Route path='/lost'>
+        <LosingPage track2={track2} />
+      </Route>
+    </BrowserRouter>
   );
 }
 
--- src/GetPlaylist.js
+++ src/GetPlaylist.js
@@ -8,17 +8,17 @@ function GetPlaylist({ playlist, tracks }) {
 
   const [leftSongIndex, setLeftSongIndex] = useState(0);
   const [rightSongIndex, setRightSongIndex] = useState(1);
-  const [trackFeed, setTrackFeed] = useState({artists: playlist.artists, images: playlist.images, tracks});
-  const [gameFeed, setGameFeed] = useState({ song1: {artist: playlist.artists[leftSongIndex], image: playlist.images[leftSongIndex], track: tracks[leftSongIndex]},
-                                             song2: {artist: playlist.artists[rightSongIndex], image: playlist.images[rightSongIndex], track: tracks[rightSongIndex]}});
+  const [trackFeed, setTrackFeed] = useState({ images: playlist.images, tracks });
+  const [gameFeed, setGameFeed] = useState({ song1: {artist: tracks[leftSongIndex].artist, image: playlist.images[leftSongIndex], track: tracks[leftSongIndex]},
+                                             song2: {artist: tracks[rightSongIndex].artist, image: playlist.images[rightSongIndex], track: tracks[rightSongIndex]}});
   const [highScore, setHighScore] = useState(0);
   const [currentHighScore, setCurrentHighScore] = useState(0);
 
   const checkHigher = () => {
     setLeftSongIndex(leftSongIndex + 1);
     setGameFeed({
-      song1: { artist: playlist.artists[leftSongIndex], image: playlist.images[leftSongIndex], track: tracks[leftSongIndex] },
-      song2: { artist: playlist.artists[rightSongIndex], image: playlist.images[rightSongIndex], track: tracks[rightSongIndex] }
+      ssong1: { artist: tracks[leftSongIndex].artist, image: playlist.images[leftSongIndex], track: tracks[leftSongIndex] },
+      song2: { artist: tracks[rightSongIndex].artist, image: playlist.images[rightSongIndex], track: tracks[rightSongIndex] }
     })
     console.log(leftSongIndex)
   }
@@ -29,7 +29,7 @@ function GetPlaylist({ playlist, tracks }) {
         <div className="song_tile">
           <div className="left_song">
             <img src={gameFeed.song1.image.url}></img>
-            <div className="text">{gameFeed.song1.track.name} by {gameFeed.song1.artist.name}</div>
+            <div className="text">{gameFeed.song1.track.name} by {gameFeed.song1.artist}</div>
             <div className="text">Has a Popularity Score of: </div>
             <div className="text">{gameFeed.song1.track.popularity}</div>
           </div>
@@ -37,7 +37,7 @@ function GetPlaylist({ playlist, tracks }) {
         <div className="circle">VS</div>
         <div className="right_song">
           <img src={gameFeed.song2.image.url}></img>
-          <div className="text">{gameFeed.song2.track.name} by {gameFeed.song2.artist.name}</div>
+          <div className="text">{gameFeed.song2.track.name} by {gameFeed.song2.artist}</div>
           <div>
             <button className="game_button" onClick={checkHigher}>Higher</button>
           </div>
@@ -49,7 +49,6 @@ function GetPlaylist({ playlist, tracks }) {
           </div>
           <div className="text">Popularity Score than {gameFeed.song1.track.name}</div>
         </div>
-        {/* </div> */}
         <div className="high_score_container">
           <div className="high_score">High Score: {highScore}</div>
           <div className="current_high_score">Current High Score: {currentHighScore}</div>
