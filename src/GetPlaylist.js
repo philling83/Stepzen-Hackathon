da7f4a0172a043487da5bf252c6cfc72449def16
philling83 <philipling83@gmail.com>
2021-04-21 22:17:29 -0400

    Fix text, display buttons, work on styling

--- src/GetPlaylist.js
+++ src/GetPlaylist.js
@@ -3,9 +3,14 @@ import React, { useState } from 'react';
 
 function GetPlaylist({ playlist, tracks }) {
 
-  const [trackFeed, setTrackFeed] = useState({artists: playlist.artists, images: playlist.images, tracks})
-  const [gameFeed, setGameFeed] = useState({ song1: {artist: playlist.artists[0], image: playlist.images[0], track: tracks[0]},
-                                             song2: {artist: playlist.artists[1], image: playlist.images[1], track: tracks[1]}})
+  let i = 0;
+  let j = 1;
+
+  const [trackFeed, setTrackFeed] = useState({artists: playlist.artists, images: playlist.images, tracks});
+  const [gameFeed, setGameFeed] = useState({ song1: {artist: playlist.artists[i], image: playlist.images[i], track: tracks[i]},
+                                             song2: {artist: playlist.artists[j], image: playlist.images[j], track: tracks[j]}});
+  const [highScore, setHighScore] = useState(0);
+  const [currentHighScore, setCurrentHighScore] = useState(0);
 
   return (
     <>
@@ -13,16 +18,32 @@ function GetPlaylist({ playlist, tracks }) {
         <div className="song_tile">
           <div className="left_song">
             <img src={gameFeed.song1.image.url}></img>
-            <div className="text">{gameFeed.song1.track.name}</div>
-            <div className="text">{gameFeed.song1.artist.name}</div>
+            <div className="text">{gameFeed.song1.track.name} by {gameFeed.song1.artist.name}</div>
+            {/* <div className="text">{gameFeed.song1.artist.name}</div> */}
+            <div className="text">Has a Popularity Score of: </div>
+            <div className="text">{gameFeed.song1.track.popularity}</div>
           </div>
         </div>
-        <div className="song_tile">
-          <div className="right_song">
-            <img src={gameFeed.song2.image.url}></img>
-            <div className="text">{gameFeed.song2.track.name}</div>
-            <div className="text">{gameFeed.song2.artist.name}</div>
+        <div className="circle">VS</div>
+        {/* <div className="song_tile"> */}
+        <div className="right_song">
+          <img src={gameFeed.song2.image.url}></img>
+          <div className="text">{gameFeed.song2.track.name} by {gameFeed.song2.artist.name}</div>
+          <div>
+            <button className="game_button">Higher</button>
           </div>
+          <div>
+            <button className="game_button">Lower</button>
+          </div>
+          <div>
+            <button className="game_button">Tied?!</button>
+          </div>
+          <div className="text">Popularity Score than {gameFeed.song1.track.name}</div>
+        </div>
+        {/* </div> */}
+        <div className="high_score_container">
+          <div className="high_score">High Score: {highScore}</div>
+          <div className="current_high_score">Current High Score: {currentHighScore}</div>
         </div>
       </div>
     </>
